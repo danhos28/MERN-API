5635db754f1a33956149923ef2f4199ddab9b854
danhos28 <danhos2810@gmail.com>
2021-03-16 12:17:16 +0700

    ini project setup server menggunakan express install nodemon

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(() => {
+    console.log('hello server');
+    console.log('hello server 2');
+    console.log('hello server 3');
+})
+
+app.listen(4000);
